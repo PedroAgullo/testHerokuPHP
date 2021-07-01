3890c17dd235fb028a05335740daafa4f4970a8b
Pedro <Pedro.agullo.marco@gmail.com>
2021-07-01 13:24:24 +0200

    Changed conection settings

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,7 +1,7 @@
 <?php 
 function conectar(){
 //Conexion con base de datos
-$mysqli = new mysqli("eu-cdbr-west-01.cleardb.com:3306", "b0c3668cd3f5e3","79f62f79", "heroku_6da6155765114ce");
+$mysqli = new mysqli("eu-cdbr-west-01.cleardb.com:3310", "b0c3668cd3f5e3","79f62f79", "heroku_6da6155765114ce");
 if ($mysqli->connect_errno) {
     echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
